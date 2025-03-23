54578757570657f68b8cce483ff34bf6846e1bf3
Jack Hunter <jhunter5@binghamton.edu>
2025-03-23 00:01:52 -0400

    packaged with electron

---
+++ mint-cat-app/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    mainWindow.loadURL('http://localhost:3000'); // Change this later for production
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
